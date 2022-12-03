a9ee8760cdfca62785f09619d833cbe9bd964a4b
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2022-12-03 12:15:45 -0800

    added heading error correction

--- src/main/java/frc/robot/commands/NavxDriveCommand.java
+++ src/main/java/frc/robot/commands/NavxDriveCommand.java
@@ -4,21 +4,51 @@
 
 package frc.robot.commands;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.DriveTrainSubsystem;
 
 public class NavxDriveCommand extends CommandBase {
   /** Creates a new NavxCommand. */
-  public NavxDriveCommand() {
+  private DriveTrainSubsystem m_driveSubsystem;
+  private double autoSpeed;
+  private double targetYaw;
+
+  public NavxDriveCommand(DriveTrainSubsystem driveSubsystem, double initAutoSpeed) {
     // Use addRequirements() here to declare subsystem dependencies.
+    m_driveSubsystem = driveSubsystem;
+    autoSpeed = initAutoSpeed;
+    addRequirements(driveSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    //caution TO-DO WARNING this effects the whole navx
+    //future note: keep track of targets inside command
+    RobotContainer.ahrs.zeroYaw();
+    targetYaw = RobotContainer.ahrs.getYaw();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    double currentYaw = RobotContainer.ahrs.getYaw();
+    double yawError = currentYaw - targetYaw;
+    double diffPow = (1 / 180) * yawError;
+    if (diffPow > 0.5) {
+      diffPow = 0.5;
+    } else if (diffPow < -0.5) {
+      diffPow = -0.5;
+    }
+    double magnitude = Constants.AUTO_SPEED_2;
+    double leftStickValue = magnitude + diffPow;
+    double rightStickValue = magnitude - diffPow;
+    DriveTrainSubsystem.setAutoSpeeds(leftStickValue, rightStickValue);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
